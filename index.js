048238fdaf84fbf7cc1a7d14fc3cb38dcae700ae
Pankaj <mpankajmandall@gmail.com>
2023-01-04 07:17:22 +0530

    Import our own created module

---
+++ index.js
@@ -0,0 +1,14 @@
+/* const app = require("./App");
+
+console.log(app)
+//-->>> require("./App") return objects of two function as two function are exportting
+
+
+console.log(app.add(5,5)); */
+
+//-->Directly access the functions from object
+const {name,add,sub} = require("./App");
+
+console.log(add(5,5))
+console.log(sub(5,5));
+console.log(name);
