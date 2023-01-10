b09a041ede531c3344af1aa7c240d83b263d36b4
Pankaj Mandal <mpankajmandall@gmail.com>
2023-01-10 10:46:05 -0800

    setInterval and clearInterval

--- Interval.js
+++ Interval.js
@@ -8,8 +8,13 @@ const updateX = ()=>{
     x+=5;
 }
 
+const del = ()=>{
+    clearInterval(intervalObj)
+}
+
 const updateY = ()=>y+=10;
 
 setInterval(updateX,500);
 setInterval(updateY,1000);
-setInterval(disp,1500);
+const intervalObj = setInterval(disp,1500);
+setTimeout(del,15000);   //stop calling only disp function again and again after 5000 ms
